866472428744f5472947b551ad9e416744d58752
Hernan Zamora <hernanzamora1999@gmail.com>
2023-07-12 16:15:54 -0300

    Fixed issues described by reviewer

--- database/schema.sql
+++ database/schema.sql
@@ -1,12 +1,3 @@
-CREATE TABLE items (
-  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  genre_id INT REFERENCES genres(id),
-  author_id INT REFERENCES authors(id),
-  label_id INT REFERENCES labels(id),
-  publish_date DATE,
-  archived BOOLEAN
-)
-
 CREATE TABLE labels (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   title VARCHAR(255),
@@ -15,13 +6,7 @@ CREATE TABLE labels (
 
 CREATE TABLE books (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  item_id INT REFERENCES items(id),
+  publish_date DATE,
   publisher VARCHAR(255),
   cover_state VARCHAR(255)
-)
-
-CREATE TABLE labels_items (
-  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  item_id INT REFERENCES items (id),
-  label_id INT REFERENCES labels (id)
 )
