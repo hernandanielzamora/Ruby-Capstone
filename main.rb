218c85b392e92e819bb41c4f2e042c3018379f82
TUMBU BARTHOLOMEW ELAD <tumbu.elad@gmail.com>
2023-07-13 16:35:10 -0700

    Upadted the main.rb file

--- main.rb
+++ main.rb
@@ -1,18 +1,28 @@
 require_relative 'src/handlers/book_handler'
 require_relative 'src/handlers/label_handler'
-require_relative 'src/handlers/games_handler'
 require_relative 'src/handlers/json_handler'
+require_relative 'src/handlers/music_genre_handler'
+require_relative 'src/handlers/games_handler'
+require_relative 'src/handlers/author_handler'
 
 class Main
   def initialize
     @book_handler = BookHandler.new
     @label_handler = LabelHandler.new
+    @music_album_handler = MusicAlbumHandler.new
+    @genre_handler = GenreHandler.new
     @games_handler = GamesHandler.new
+    @author_handler = AuthorHandler.new
     @storage = HandleJson.new('./JSON/')
     @book_handler.book_json = @storage.load_from_json('books')
     @label_handler.label_json = @storage.load_from_json('labels')
+    @music_album_handler.music_album_json = @storage.load_from_json('music_albums')
+    @genre_handler.genre_json = @storage.load_from_json('genres')
     @games_handler.games_json = @storage.load_from_json('games')
+    @author_handler.author_json = @storage.load_from_json('authors')
     @label_handler.init_labels_list
+    @genre_handler.init_genres_list
+    @author_handler.init_authors_list
     puts '----------------------------------------'
     puts 'Be welcomed to cathalog all my things'
     puts
@@ -25,11 +35,17 @@ class Main
   def storage_data
     book_json = @book_handler.book_json
     label_json = @label_handler.label_json
+    music_album_json = @music_album_handler.music_album_json
+    genre_json = @genre_handler.genre_json
     games_json = @games_handler.games_json
+    author_json = @author_handler.author_json
 
     @storage.storage_in_json('books', book_json)
     @storage.storage_in_json('labels', label_json)
+    @storage.storage_in_json('music_albums', music_album_json)
+    @storage.storage_in_json('genres', genre_json)
     @storage.storage_in_json('games', games_json)
+    @storage.storage_in_json('authors', author_json)
   end
 
   def display_options
@@ -61,26 +77,79 @@ class Main
   # 9 => 'option_9',
   # 10 => :exit_program
 
+  # def selected_option(chosen)
+  #   loop do
+  #     case chosen
+  #     when 1
+  #       @book_handler.list_book
+  #       puts ''
+  #     when 2
+  #       @music_album_handler.list_music_album
+  #     when 3
+  #       @games_handler.list_games
+  #     when 4
+  #       @genre_handler.list_genres
+  #     when 5
+  #       @label_handler.list_labels
+  #     when 6
+  #       @author_handler.list_authors
+  #     when 7
+  #       @book_handler.add_book(@label_handler)
+  #     when 8
+  #       @music_album_handler.add_music_album(@genre_handler)
+  #     when 9
+  #       @games_handler.add_games(@author_handler)
+  #     when 10
+  #       puts 'Thank you. Exiting program'
+  #       storage_data
+  #       break
+  #     else
+  #       puts 'Invalid option. Please select a number from the menu'
+  #     end
+  #     display_options
+  #     chosen = get_input('Select an option:').to_i
+  #   end
+  # end
+
   def selected_option(chosen)
+    options = {
+      1 => { handler: @book_handler, method: :list_book },
+      2 => { handler: @music_album_handler, method: :list_music_album },
+      3 => { handler: @games_handler, method: :list_games },
+      4 => { handler: @genre_handler, method: :list_genres },
+      5 => { handler: @label_handler, method: :list_labels },
+      6 => { handler: @author_handler, method: :list_authors },
+      7 => { handler: @book_handler, method: :add_book, params: [@label_handler] },
+      8 => { handler: @music_album_handler, method: :add_music_album, params: [@genre_handler] },
+      9 => { handler: @games_handler, method: :add_games, params: [@author_handler] },
+      10 => :exit_program
+    }
+
     loop do
-      case chosen
-      when 1
-        @book_handler.list_book
-        puts ''
-      when 5
-        @label_handler.list_labels
-      when 7
-        @book_handler.add_book(@label_handler)
-      when 10
-        puts 'Thank you. Exiting program'
-        storage_data
-        break
+      if options.key?(chosen)
+        option = options[chosen]
+        if option == :exit_program
+          puts 'Thank you. Exiting program'
+          storage_data
+          break
+        else
+          handle_option(option)
+        end
       else
         puts 'Invalid option. Please select a number from the menu'
       end
+
       display_options
       chosen = get_input('Select an option:').to_i
     end
+  end
+
+  def handle_option(option)
+    handler = option[:handler]
+    method = option[:method]
+    params = option[:params] || []
+    handler.send(method, *params)
+    puts ''
   end
 end
 
