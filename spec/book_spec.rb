0e78cc53db8ef767d45299f188573181ed8140b8
Hernan Zamora <hernanzamora1999@gmail.com>
2023-07-13 10:46:18 -0300

    Fixed Tests - Tested application (Everything still works fine)

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,7 +2,7 @@ require_relative '../src/classes/book'
 
 describe Book do
   before :each do
-    @book = Book.new('publisher', 'cover_state', '2020/05/04')
+    @book = Book.new('2020-05-04', 'publisher', 'cover_state')
   end
 
   describe '#new' do
@@ -18,15 +18,15 @@ describe Book do
 
   describe '#can_be_archived?' do
     it 'Return true if > 10years' do
-      book = Book.new('publisher', 'cover_state', '2002/2/2')
+      book = Book.new('2002-2-2', 'publisher', 'cover_state')
       expect(book.can_be_archived?).to be_truthy
     end
     it 'Return true if cover state is bad' do
-      book = Book.new('publisher', 'bad', '2021/1/1')
+      book = Book.new('2021-1-1', 'publisher', 'bad')
       expect(book.can_be_archived?).to be_truthy
     end
     it 'should return false if date is not older than 10 years and cover_state is not bad' do
-      book = Book.new('publisher', 'good', '2023/04/05')
+      book = Book.new('2023-04-05', 'publisher', 'good')
       expect(book.can_be_archived?).to be_falsy
     end
   end
