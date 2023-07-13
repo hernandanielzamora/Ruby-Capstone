d73ab103e50a38b43a8a543074a91259bcae6be8
TUMBU BARTHOLOMEW ELAD <tumbu.elad@gmail.com>
2023-07-13 13:52:15 -0700

    Upadted the author.rb file

--- src/classes/author.rb
+++ src/classes/author.rb
@@ -1,32 +1,17 @@
-require 'json'
-
-# Represents an author of items.
 class Author
-  attr_reader :id, :first_name, :last_name
-  attr_accessor :items
+  attr_reader :id, :first_name, :last_name, :items
 
-  def initialize(id, first_name, last_name)
-    @id = id
+  def initialize(first_name, last_name)
+    @id = Random.rand(1...1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
   end
 
   def add_item(item)
+    raise TypeError, 'Invalid type, must be an Item instance' unless item.is_a?(Item)
+
     @items << item
-    item.author = self
-  end
-
-  def self.load_authors(filename)
-    JSON.parse(File.read(filename), symbolize_names: true).map do |author_data|
-      Author.new(author_data[:id], author_data[:first_name], author_data[:last_name])
-    end
-  end
-
-  def self.save_authors(authors, filename)
-    authors_data = authors.map do |author|
-      { id: author.id, first_name: author.first_name, last_name: author.last_name }
-    end
-    File.write(filename, JSON.dump(authors_data))
+    item.insert_author(self)
   end
 end
