894f6bca07add114c3b36e9d39ef992729658d42
TUMBU BARTHOLOMEW ELAD <tumbu.elad@gmail.com>
2023-07-13 14:00:52 -0700

    Upadted the handlers folder

---
+++ src/handlers/games_handler.rb
@@ -0,0 +1,66 @@
+require_relative '../classes/games'
+require_relative 'author_handler'
+
+class GamesHandler
+  attr_accessor :games_list, :games_json
+
+  def initialize
+    @games_list = []
+    @games_json = []
+  end
+
+  def games_to_json(games)
+    {
+      'id' => games.id,
+      'game_name' => games.game_name,
+      'multiplayer' => games.multiplayer,
+      'publish_date' => games.publish_date,
+      'last_played_at' => games.last_played_at
+    }
+  end
+
+  def list_games
+    if games_json.empty?
+      puts 'There are no games in the list'
+    else
+      puts ''
+      puts 'Games: '
+      puts ''
+      @games_json.each_with_index do |games, i|
+        id = games['id']
+        name = games['name']
+        multiplayer = games['multiplayer']
+        publish_date = games['publish_date']
+        last_played_at = games['last_played_at']
+        puts "#{i + 1}|ID: #{id} - name: #{name} - Cover State: #{multiplayer} - Publish Date: #{publish_date} - Last Played At: #{last_played_at}"
+      end
+      puts ''
+    end
+    puts '----------------------------------------'
+  end
+
+  def add_games(author_handler)
+    puts 'Please insert the following information!'
+    puts ''
+    print 'Game`s name: '
+    games_name = gets.chomp
+    print 'Multiplayer game? (true/false): '
+    games_multiplayer = gets.chomp
+    puts ''
+    print 'Publish date (YYYY-MM-DD): '
+    games_date = gets.chomp
+    print 'Last time you played it? (YYYY-MM-DD): '
+    games_last_played_at = gets.chomp
+    # Validate the date format
+    if games_date.match(/\A\d{4}-\d{2}-\d{2}\z/)
+      games = Games.new(games_name, games_multiplayer, games_date, games_last_played_at)
+      @games_list.push(games)
+      @games_json.push(games_to_json(games))
+      author_handler.add_author(games)
+      puts 'Game added!'
+    else
+      puts 'Invalid date format. Please use the format YYYY-MM-DD.'
+    end
+    puts ''
+  end
+end
